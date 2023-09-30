5e3dd654d1a18394b8f6eb11ae3a7c109fc5bad5
Froxss <burakyetis@gmail.com>
2023-09-30 14:44:42 +0300

    Şifre oluşturucu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,58 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password';
+  title = 'Şifre Oluşturucu';
+
+  lettersBox: boolean = false;
+  numbersBox: boolean = false;
+  symbolsBox: boolean = false;
+  length: number = 0;
+  password = '';
+
+  passLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    } else {
+      alert('Lütfen Geçerli bir sayi giriniz');
+    }
+  }
+
+  lettersCheck() {
+    this.lettersBox = !this.lettersBox;
+  }
+  numbersCheck() {
+    this.numbersBox = !this.numbersBox;
+  }
+  symbolsCheck() {
+    this.symbolsBox = !this.symbolsBox;
+  }
+
+  btnClick() {
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+
+    let chars = '';
+    if (this.lettersBox) {
+      chars += letters;
+    }
+    if (this.numbersBox) {
+      chars += numbers;
+    }
+    if (this.symbolsBox) {
+      chars += symbols;
+    }
+
+    let generatedPass = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * chars.length);
+      generatedPass += chars[index];
+      this.password = generatedPass;
+    }
+  }
 }
